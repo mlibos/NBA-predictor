318ced42ff6f8ec38bbcbe905de45c56b1aa27b6
mlibos <munir.libos@gmail.com>
2020-12-19 13:27:33 -0600

    updated databases

--- model.py
+++ model.py
@@ -5,11 +5,11 @@ import math
 import random
 import time
 import pandas
+import numpy
 
 
-
-data_files = []
 #list out data files in data folder and save them to new variable names for easy access
+data_files = []
 entries = Path('C:/Users/SEDan/Documents/github/NBA-predictor/data/')
 for entry in entries.iterdir():
 	file = 'C:/Users/SEDan/Documents/github/NBA-predictor/data/' + entry.name
@@ -19,5 +19,17 @@ advanced_stats = pandas.read_csv(data_files[0])
 per_game_stats = pandas.read_csv(data_files[1])
 team_per_game = pandas.read_csv(data_files[2])
 team_total = pandas.read_csv(data_files[3])
+team_per_game.set_index('Team',inplace=True)
+team_total.set_index('Team',inplace = True)
+advanced_stats.set_index('Player',inplace=True)
+per_game_stats.set_index('Player',inplace=True)
 
-# print(advanced_stats,'\n',per_game_stats,'\n',team_per_game,'\n',team_total)
+#dictionary of teams where each key is a team and each value is a list of roster players updated on 12/19/20
+team_rosters = {}
+for team in team_per_game.index:
+	team_rosters[team] = []
+print(team_rosters)
+print(advanced_stats)
+print(per_game_stats)
+print(team_total)
+print(team_per_game)
