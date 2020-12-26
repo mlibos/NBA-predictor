c46e7647036d3762f5be500c28f250192db8ed78
mlibos <munir.libos@gmail.com>
2020-12-25 18:13:11 -0600

    Update model.py

--- model.py
+++ model.py
@@ -47,7 +47,7 @@ def simulate_game(home_team,away_team,elos):
 	home_elo = elos[home_team]
 	away_elo = elos[away_team]
 	#home_field_advantage in bball is ? I'm guessing its worth a solid 20 elo in covid-19 times
-	home_elo += 15
+	home_elo += 20
 	expected_home = 1/(1+10**((away_elo-home_elo)/400))
 	expected_away = 1-expected_home
 	event = random.random()
@@ -89,7 +89,7 @@ def simulate_season(eastern_conference,western_conference,elos,records,schedule)
 	return records
 start = time.time()
 total_records = {'Atlanta Hawks': [0, 0], 'Boston Celtics': [0, 0], 'Brooklyn Nets': [0, 0], 'Charlotte Hornets': [0, 0], 'Chicago Bulls': [0, 0], 'Cleveland Cavaliers': [0, 0], 'Dallas Mavericks': [0, 0], 'Denver Nuggets': [0, 0], 'Detroit Pistons': [0, 0], 'Golden State Warriors': [0, 0], 'Houston Rockets': [0, 0], 'Indiana Pacers': [0, 0], 'Los Angeles Clippers': [0, 0], 'Los Angeles Lakers': [0, 0], 'Memphis Grizzlies': [0, 0], 'Miami Heat': [0, 0], 'Milwaukee Bucks': [0, 0], 'Minnesota Timberwolves': [0, 0], 'New Orleans Pelicans': [0, 0], 'New York Knicks': [0, 0], 'Oklahoma City Thunder': [0, 0], 'Orlando Magic': [0, 0], 'Philadelphia 76ers': [0, 0], 'Phoenix Suns': [0, 0], 'Portland Trail Blazers': [0, 0], 'Sacramento Kings': [0, 0], 'San Antonio Spurs': [0, 0], 'Toronto Raptors': [0, 0], 'Utah Jazz': [0, 0], 'Washington Wizards': [0, 0]}
-n = 1000
+n = 150
 for i in range(n):
 	#player based elo on december 17 2020 (before season started) based on 538
 	elos = {'Atlanta Hawks': 1536, 'Boston Celtics': 1602, 'Brooklyn Nets': 1578, 'Charlotte Hornets': 1396, 'Chicago Bulls': 1421, 'Cleveland Cavaliers': 1380, 'Dallas Mavericks': 1584, 'Denver Nuggets': 1613, 'Detroit Pistons': 1354, 'Golden State Warriors': 1488, 'Houston Rockets': 1607, 'Indiana Pacers': 1537, 'Los Angeles Clippers': 1637, 'Los Angeles Lakers': 1658, 'Memphis Grizzlies': 1515, 'Miami Heat': 1594, 'Milwaukee Bucks': 1632, 'Minnesota Timberwolves': 1483, 'New Orleans Pelicans': 1552, 'New York Knicks': 1382, 'Oklahoma City Thunder': 1466, 'Orlando Magic': 1512, 'Philadelphia 76ers': 1603, 'Phoenix Suns': 1604, 'Portland Trail Blazers': 1559, 'Sacramento Kings': 1503, 'San Antonio Spurs': 1495, 'Toronto Raptors': 1607, 'Utah Jazz': 1590, 'Washington Wizards': 1430}
